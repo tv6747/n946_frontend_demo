82fffa8f204096d80b1856dea8dfb906535281ff
6747 <shiyu.ye@tradevan.com.tw>
2026-01-27 08:52:53 +0800

    fix: update homepage url with trailing slash

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/n946_frontend_demo/', // 必須跟你的倉庫名稱一致，且前後都有斜線
 })
